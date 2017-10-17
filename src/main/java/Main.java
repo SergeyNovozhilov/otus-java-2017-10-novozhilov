5cbfe786076de6f61a8a564300a61e14a84308f9
Sergey Novozhilov <sergey.novozhilov@netcracker.com>
2017-10-17 16:22:58 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,24 @@
+import com.google.common.collect.Ordering;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
+    private static int MAX_NUMBER = 100;
+
     public static void main(String ... args) {
-        
+        List<Integer> intList = new ArrayList<>();
+        for (int i = 0; i < MAX_NUMBER; i++) {
+            intList.add((int)(Math.random() * MAX_NUMBER + 1));
+        }
+        Ordering ordering = Ordering.natural();
+        System.out.println("Unsorted:");
+        System.out.println(intList);
+        Collections.sort(intList, ordering);
+        System.out.println("Sorted:");
+        System.out.println(intList);
     }
 
 }
