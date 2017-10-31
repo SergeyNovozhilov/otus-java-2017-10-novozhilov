123f11d78c4913f0b52c3c594e7db89a0c40555d
SergeyNovozhilov <sergey.n.novozhilov@gmail.com>
2017-10-31 22:50:21 +0400

    clean

--- src/main/java/ru/otus/Main.java
+++ src/main/java/ru/otus/Main.java
@@ -1,9 +1,6 @@
 package ru.otus;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -14,23 +11,5 @@ public class Main {
         }
         System.out.println("Size of String list with 10 elements: " + MemoryCalculator.calculate(list));
 
-        System.out.println("Size of empty string: " + MemoryCalculator.emptyString());
-
-        System.out.println("Size of empty string as an object: " + MemoryCalculator.calculate(""));
-
-        System.out.println("Size of 'Hello' string: " + MemoryCalculator.calculate("Hello"));
-
-        System.out.println("Size of TestClass: " + MemoryCalculator.calculate(new TestClass()));
-
-        System.out.println("Size of empty String array: " + MemoryCalculator.calculate(new String[0]));
-
-        System.out.println("Size of String array with 10 elements: " + MemoryCalculator.calculate(new String[10]));
-
-        Set<String> set = new HashSet<>();
-        System.out.println("Size of empty String set: " + MemoryCalculator.calculate(set));
-        for (int i = 0; i < 10; i++) {
-            set.add("Hello " + i);
-        }
-        System.out.println("Size of String set with 10 elements: " + MemoryCalculator.calculate(list));
     }
 }
--- src/main/java/ru/otus/TestClass.java
+++
@@ -1,7 +0,0 @@
-package ru.otus;
-
-public class TestClass {
-    private String s1 = "Hello";
-    private int i = 0;
-    private long l = 0l;
-}
