a8f20ef50843efc81a982b95126a07a527c2abbf
Sergey Novozhilov <sergey.novozhilov@netcracker.com>
2017-11-01 11:40:14 +0300

    test

--- src/main/java/ru/otus/Main.java
+++ src/main/java/ru/otus/Main.java
@@ -10,7 +10,7 @@ public class Main {
         List<String> list = new ArrayList<>();
         System.out.println("Size of empty String list: " + MemoryCalculator.calculate(list));
         for (int i = 0; i < 10; i++) {
-            list.add("Hello " + i);
+            list.add("Hello: " + i);
         }
         System.out.println("Size of String list with 10 elements: " + MemoryCalculator.calculate(list));
 
