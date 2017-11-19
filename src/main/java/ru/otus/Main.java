779ef541993870bdf8f9dfbcd18a179a2cc9c9fd
SergeyNovozhilov <sergey.n.novozhilov@gmail.com>
2017-11-19 16:39:27 +0300

    HW05 solution +

--- src/main/java/ru/otus/Main.java
+++ src/main/java/ru/otus/Main.java
@@ -1,16 +1,6 @@
 package ru.otus;
 
-import com.google.common.reflect.ClassPath;
 import ru.otus.Tester.Tester;
-import ru.otus.annotations.After;
-import ru.otus.annotations.Before;
-import ru.otus.annotations.Test;
-
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class Main {
--- src/main/java/ru/otus/ObjectClassTest.java
+++ src/main/java/ru/otus/ObjectClassTest.java
@@ -1,6 +1,5 @@
 package ru.otus;
 
-import ru.otus.Tester.TestFailedException;
 import ru.otus.annotations.After;
 import ru.otus.annotations.Before;
 import ru.otus.annotations.Test;
@@ -10,39 +9,41 @@ public class ObjectClassTest {
     ObjectClass oc = null;
 
     @Before
-    public void prepare1() {
-        System.out.println("Before 1 ");
-        oc = new ObjectClass();
-        oc.setField("");
-    }
-
-    @Before
     public void prepare2() {
         System.out.println("Before 2 ");
         oc = new ObjectClass();
         oc.setField("Object");
     }
 
+    @Before
+    public void prepare1() {
+        System.out.println("Before 1 ");
+        oc = new ObjectClass();
+        oc.setField("");
+    }
+
+
+
     @Test
-    public void runTest1() throws TestFailedException {
+    public void runTest1() throws Exception {
         System.out.println("Test1");
         if (!oc.getField().equals("Object")) {
-            throw new TestFailedException(" Test 1 Failed");
+            throw new Exception(" Test 1 Failed");
         }
     }
 
     @Test
-    public void runTest2() throws TestFailedException {
+    public void runTest2() throws Exception {
         System.out.println("Test2");
         if (!oc.getField().equals("")) {
-            throw new TestFailedException(" Test 2 Failed");
+            throw new Exception(" Test 2 Failed");
         }
     }
 
     @Test
-    public boolean runTest3() throws TestFailedException{
+    public boolean runTest3() throws Exception{
         System.out.println("Test3");
-        throw new TestFailedException("Test3 always failed");
+        throw new Exception("Test 3 always failed");
     }
 
     @After
