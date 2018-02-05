4034aa85e53277380a274868ab3eccfb1f66f25a
Sergey Novozhilov <sergey.novozhilov@netcracker.com>
2018-02-05 15:32:25 +0300

    commit 2

--- src/main/java/ru/otus/web/servlet/AdminServlet.java
+++ src/main/java/ru/otus/web/servlet/AdminServlet.java
@@ -2,21 +2,37 @@ package ru.otus.web.servlet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import ru.otus.database.DataSet.AddressDataSet;
+import ru.otus.database.DataSet.PhoneDataSet;
+import ru.otus.database.DataSet.UserDataSet;
 import ru.otus.database.DbService.DbService;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
 public class AdminServlet extends HttpServlet {
 
-    @Autowired
-    private DbService db;
 
+    private final DbService db;
+
+    @Autowired
+    public AdminServlet(DbService db) {
+        this.db = db;
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+
+    }
 
     private Map<String, Object> createPageVariablesMap(String message) {
         Map<String, Object> pageVariables = new HashMap<>();
@@ -42,6 +58,7 @@ public class AdminServlet extends HttpServlet {
         } else {
             Map<String, Object> pageVariables;
             if (LoginServlet.checkAdmin(requestLogin, requestPassword)) {
+                sebDb(db);
                 pageVariables = createPageVariablesMap(null);
             } else {
                 pageVariables = createPageVariablesMap("User " + requestLogin + " is not authorized to monitor data. ");
@@ -52,5 +69,39 @@ public class AdminServlet extends HttpServlet {
 
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    private void sebDb(DbService ds) {
+        System.out.println("setDb");
+
+        String street = "Ark street";
+        AddressDataSet address = new AddressDataSet(street);
+        UserDataSet user = new UserDataSet("Jones", 27, address, null);
+        List<PhoneDataSet> phones = Arrays.asList(new PhoneDataSet("110-12-23", user), new PhoneDataSet("113-23-34", user));
+        user.setPhones(phones);
+        ds.save(user);
+        UserDataSet userRead1 = ds.load(1, UserDataSet.class);
+        if (userRead1 == null) {
+            System.out.println("Error! userRead1 is null");
+        }
+
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        UserDataSet userRead2 = ds.load(1, UserDataSet.class);
+        if (userRead2 == null) {
+            System.out.println("Error! userRead2 is null");
+        }
+        try {
+            Thread.sleep(400);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        UserDataSet userRead3 = ds.load(1, UserDataSet.class);
+        if (userRead3 == null) {
+            System.out.println("Error! userRead3 is null");
+        }
     }
 }
